2e3c5ef69728b4f5871943d3477d2000732d79b9
lewin <2438124738@qq.com>
2020-04-08 22:25:52 +0800

    fix get方法中的%20问题

--- src/database_process.c
+++ src/database_process.c
@@ -90,6 +90,24 @@ void database_process(void* arg){
     temp[i+1] = '\0';
     sscanf(temp, "%d", &scale);
 
+    //对于通过http协议传输的时间，空格会被‘+’或 %20 取代
+    //所以需要对收到的字符串进行处理
+    printf("%s\n", re_time);
+    int x, y;
+    for(x = 0, y = 0; re_time[y]; x++, y++){
+        if(re_time[y] == '+')
+            re_time[x] = ' ';
+        else if(re_time[y] == '%'){
+            if(!(re_time[y+1]&&re_time[y+2]))
+                break;
+            re_time[x] = ' ';
+            y += 2;
+        }
+        else
+            re_time[x] = re_time[y];
+    }
+    re_time[x] = '\0';
+
     //printf("%ld\n%s\n%lf\n%lf\n%d\n", numb, re_time, latitude, longitude, scale);
     
 /*
